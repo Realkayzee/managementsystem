186a150c6b6d215146cc6618227eca0ddbe77130
kayzee <olaniyikazeem112@gmail.com>
2022-10-28 01:18:17 +0100

    initial frontend commit

---
+++ management-frontend/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+
+
+
+function App({ Component, pageProps }: AppProps) {
+  return(
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+
+export default App
